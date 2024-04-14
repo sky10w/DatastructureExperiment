d05b78a0ac2a2ff42582cc09a3ddec4796789221
sky10w <sky10w@163.com>
2024-04-14 21:41:43 +0800

    Add Action&Buff string parser - 2024_4_14

--- InGame/Stageconfig.cpp
+++
@@ -1,3 +0,0 @@
-#include "Stageconfig.h"
-
-StageConfig::StageConfig() {}
--- InGame/styles.cpp
+++
@@ -1,8 +0,0 @@
-#include "styles.h"
-
-const QString InGameStyle::overallStyle = {
-	".Card {"
-	"	background: #CA6F1E;"
-	"	border: 3px solid black;"
-	"}"
-};
